0bd1c6e6884b755f82f181a05a9fd56bbe9b453f
William Karlström <wkarlstrom@outlook.com>
2023-10-04 14:55:38 +0200

    Add mySql.js Add connection in app.js Add mysql dependency
    Co-authored-by: Ahlberg-iths <113943951+ahlberg-iths@users.noreply.github.com>
    
    Co-authored-by: Stefan Karlsson <phtefan.karlsson@gmail.com>
    
    Co-authored-by: JesperBodin <112554269+jesperbodin@users.noreply.github.com>
    
    Co-authored-by: Arman <112554095+ArmanAlemirad@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 const port= 3000;
+const db = require('./database/mySql.js')
 
-app.get('/',(req, res) => {
-    res.send('Hello World');
+app.get('/', async (req, res) => {
+    try {
+        db.getConnection();
+        res.send('Hello World!');
+    } catch (error){
+        res.send('Error connecting..')
+    }
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log('App listening on port ${port}');
+    try {
+        await db.initializeDB();
+    } catch (error){
+        console.log('Failed to connect...')
+    }
 });
 
+//const cors to allow requests from other URL's
+//const socketIO
+//const server = http.createServer(app))
