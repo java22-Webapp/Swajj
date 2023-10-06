bc1f7cd6d5c2f08799976af1f377dd1474eef434
Ye4y <Arman.Alemirad@gmail.com>
2023-10-06 14:16:16 +0200

    Add router with two components

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,4 +1,5 @@
 <script setup>
+
 </script>
 
 <template>
@@ -6,19 +7,33 @@
 
     <section class="buttons">
       <img id="cardBrain" src="../assets/cardBrainYellow.png" alt="Brain holding a card" />
-      <button id="soloPlay">Play Solo</button>
-      <button id="multiPlay">Multiplayer</button>
+      <button  id="soloPlay">
+        <router-link to="/Solo" id="solo-play-text">Play Solo</router-link>
+      </button>
+      <button id="multiPlay">
+        <router-link to="/Multiplayer" id="multi-play-text">Multiplayer</router-link>
+      </button>
+
     </section>
   </main>
 </template>
 
+
 <style scoped>
 
+
+
+
+#solo-play-text, #multi-play-text {
+  text-decoration: none;
+  color: var(--card-color);
+}
 
 button {
   font-family: 'Inter', sans-serif;
   font-weight: bold;
   font-size: 36px;
+  text-decoration: none;
   text-shadow:
     -1px -1px 0 #000,
     1px -1px 0 #000,
@@ -32,8 +47,6 @@ button {
   z-index: 999;
 }
 
-
-
 .buttons {
   display: flex;
   justify-content: center;
@@ -41,7 +54,6 @@ button {
   flex-direction: column;
   gap: 1em;
 }
-
 
 #cardBrain {
   top: 35%;
---
+++ src/views/LobbySolo.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>LobbySolo sida</div>
+</template>
+
+<script setup></script>
+<style scoped></style>
