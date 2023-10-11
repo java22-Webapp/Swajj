90982e7502e243431ac7ace72483767c18e98e24
William Karlström <wkarlstrom@outlook.com>
2023-10-11 10:38:02 +0200

    Add cardDeck images

--- frontend/src/views/PlayView.vue
+++ frontend/src/views/PlayView.vue
@@ -1,11 +1,19 @@
 <script setup>
 import RoundCounter from '@/components/RoundCounter.vue';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
+import questionCardStack from "../assets/questionCardStack.png"
+import questionCardStackFlipped from "../assets/questionCardStackFlipped.png"
+
 
 const questions = ref('');
 const answers = ref([]);
+const imgSrc = ref(questionCardStack);
+const isFlipped = computed(() => imgSrc.value === questionCardStackFlipped);
 
 const fetchQuestionAndAnswers = async () => {
+  await new Promise(resolve => setTimeout(resolve, 3000))
+
+
   try {
     const response = await fetch('http://localhost:3000/get-question');
 
@@ -15,13 +23,17 @@ const fetchQuestionAndAnswers = async () => {
     const data = await response.json();
     questions.value = data.db_question;
     answers.value = data.db_answers;
+
+    imgSrc.value = questionCardStackFlipped;
+
     console.log("FETCHED DATA: ", data)
   } catch (error) {
     console.log('ERROR fetching questions in PlayView', error);
   }
 };
 
-onMounted(fetchQuestionAndAnswers);
+onMounted(fetchQuestionAndAnswers)
+
 </script>
 
 <template>
@@ -31,7 +43,12 @@ onMounted(fetchQuestionAndAnswers);
     </div>
   </section>
   <section class="QNA">
-    <div>{{ questions }}</div>
+    <div id="deckDiv">
+      <div class="deckQuestions">{{ questions }}</div>
+      <img id="idleDeck" :src="imgSrc" :class="{ 'flipped': isFlipped}" alt="Card deck">
+
+    </div>
+
     <div id="answerBtns">
       <button class="menuButton" id="btnAnswerA"> {{ answers[0] }}</button>
       <button class="menuButton" id="btnAnswerB"> {{ answers[1] }}</button>
@@ -44,11 +61,43 @@ onMounted(fetchQuestionAndAnswers);
 </template>
 
 <style scoped>
+
 .rotatedCardBrain {
   position: absolute;
   top: 28em;
   left: -8em;
   transform: rotate(30deg);
+}
+
+.deckQuestions {
+  position: absolute;
+  top: 60%;
+  left: 58%;
+  transform: translate(-70%, -50%);
+  z-index: 1;
+  width: fit-content;
+  font-size: 24px;
+
+}
+
+#deckDiv {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+#idleDeck {
+  width: 30%;
+  height: 50%;
+  display: block;
+}
+
+#idleDeck.flipped {
+  width: 70%;
+  left: -4%;
+  position: relative;
+
 }
 
 .showRound {
@@ -82,8 +131,6 @@ onMounted(fetchQuestionAndAnswers);
   width: 6em;
   height: 4em;
   padding: 2px;
-
-
-
 }
+
 </style>
